500eddb000a5752cd604011e6aaa32d89f3e8ce3
Roger Tu <rtu@cal.berkeley.edu>
2016-04-23 12:53:57 -0700

    Initial templating

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+# Controllers
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route("/")
 def hello():
-    return "Hello world!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))

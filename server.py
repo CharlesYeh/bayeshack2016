030f32f0e80f44aa4f7639330e97eb6a619bfcee
Charles Yeh <charlesyeh@dropbox.com>
2016-04-24 00:31:58 -0700

    Add risk calculation and MySQL connection

--- server.py
+++ server.py
@@ -1,40 +1,70 @@
+import json
 import os
 
 from flask import Flask
 from flask import render_template
+from flask import request
+
+import risk
 
 app = Flask(__name__)
+
 
 # Controllers
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.route("/")
 def root():
     return render_template('index.html')
+
 
 @app.route("/index.html")
 def index():
     return root()
 
+
 @app.route("/form1.html")
 def form1():
     return render_template('form1.html')
+
 
 @app.route("/form2.html")
 def form2():
     return render_template('form2.html')
 
+
 @app.route("/plans.html")
 def plans():
     return render_template('plans.html')
+
 
 @app.route("/details.html")
 def details():
     return render_template('details.html')
 
+
+@app.route("/fetch_stats", methods=['POST'])
+def login():
+    '''
+    Input should be:
+    {
+        'age': 20,
+        'sex': 'M',
+        'doctors': ['1235'],
+        'specialists': ['12345'],
+        'drugs': ['123']
+    }
+    '''
+    data = request.form
+    return json.dumps(risk.get_risk(
+        data['age'],
+        data['sex']))
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(host='0.0.0.0', port=port, debug=True)  # debug=True for autorestart
+    # debug=True for autorestart
+    app.run(host='0.0.0.0', port=port, debug=True)

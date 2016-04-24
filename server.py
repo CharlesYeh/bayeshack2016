3182fe78cfbd47aa6468eaac3af7b85b24b244b8
Roger Tu <rtu@cal.berkeley.edu>
2016-04-23 17:32:31 -0700

    Add some form templates

--- server.py
+++ server.py
@@ -11,9 +11,22 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 @app.route("/")
-def hello():
+def root():
     return render_template('index.html')
+
+@app.route("/index.html")
+def index():
+    return root()
+
+@app.route("/form1.html")
+def form1():
+    return render_template('form1.html')
+
+@app.route("/form2.html")
+def form2():
+    return render_template('form2.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)  # debug=True for autorestart
